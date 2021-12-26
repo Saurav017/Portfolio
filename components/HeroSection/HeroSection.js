ed3f696d1180409db0001528ca2e3383732f252d
Saurav017 <sauravabde@gmail.com>
2021-12-26 15:15:53 +0530

    HeroImage Added

--- components/HeroSection/HeroSection.js
+++ components/HeroSection/HeroSection.js
@@ -1,15 +1,44 @@
 import Image from 'next/image'
-import { Link } from 'react-scroll'
-import heroImg from '../../public/images/heroImg.png'
+import { Link as LinkS }  from 'react-scroll'
+import Link from 'next/link'
+import heroImg  from '../../public/images/heroImg.png'
 import classes from './HeroSection.module.css'
+import { FaTwitter } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
 
 const HeroSection = () => {
     return (
       <main className={classes.container}>
         <div className={classes.heroDiv}>
-            <div className={classes.imgDiv}>
-              <Image src={heroImg} alt="heroImg" layout="responsive" objectFit="contain" />
+          <div className={classes.imgDiv}>
+            <div className={classes.iconsDiv}>
+              <div className={classes.icon}>
+                <Link href="https://twitter.com/SauravPurohit4">
+                  <FaTwitter />
+                </Link>
+              </div>
+              <div className={classes.icon}>
+                <Link href="https://twitter.com/SauravPurohit4">
+                  <FaLinkedin />
+                </Link>
+              </div>
+              <div className={classes.icon}>
+                <Link href="https://twitter.com/SauravPurohit4">
+                  <AiFillGithub />
+                </Link>
+              </div>
             </div>
+            <div className={classes.imgContainer}>
+              <Image
+                src={heroImg}
+                alt="heroImg"
+                layout="responsive"
+                objectFit="contain"
+              />
+            </div>
+          </div>
+          <div className={classes.textDiv}>
           <h2 className={classes.welcome}>Hello</h2>
           <h1 className={classes.title}>
             I am <span className={classes.name}>Saurav</span>
@@ -18,7 +47,7 @@ const HeroSection = () => {
             A Front End Developer passionate to building websites and web
             applications.
           </p>
-          <Link
+          <LinkS
             to="projects"
             spy={true}
             smooth={true}
@@ -26,8 +55,9 @@ const HeroSection = () => {
             offset={0}
           >
             <button className={classes.heroBtn}>View My Work</button>
-          </Link>
-        </div>
+          </LinkS>
+          </div>
+          </div>
       </main>
     );
 }
