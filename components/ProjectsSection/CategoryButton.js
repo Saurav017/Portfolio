71afaafd7bee3640b4f7af509402dd1c5328ece4
Saurav017 <sauravabde@gmail.com>
2021-12-29 15:19:55 +0530

    Final Changes

--- components/ProjectsSection/CategoryButton.js
+++ components/ProjectsSection/CategoryButton.js
@@ -11,7 +11,7 @@ const CategoryButton = ({ filterCategory, handleFilter, id }) => {
                         {category}
                     </button>
                 )
-            })})
+            })}
         </div>
     )
 }
